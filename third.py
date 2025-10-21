1594c611190118b568c6fd69e0d4fb28c957bd4a
Basta05 <135508315+Basta05@users.noreply.github.com>
2025-10-21 20:25:12 +0200

    Add files via upload

---
+++ third.py
@@ -0,0 +1,25 @@
+def je_prvocislo(cislo):
+    if cislo < 2:
+        return True
+    for i in range(2, cislo):
+        if cislo % i == 0:
+            return False
+    return True
+    
+    
+    
+
+def vrat_prvocisla(maximum):
+
+    maximum = int(maximum)
+    seznam_prvocisla = []
+
+    for n in range(2, maximum + 1):
+        if je_prvocislo(n):
+            seznam_prvocisla.append(n)
+    return seznam_prvocisla
+
+if __name__ == "__main__":
+    cislo = input("Zadej maximum: ")
+    prvocisla = vrat_prvocisla(cislo)
+    print(prvocisla)
