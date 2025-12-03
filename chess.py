0503c072a2809f3ab004b7e016ab3041a8ff6054
Bašta <135508315+Basta05@users.noreply.github.com>
2025-12-03 14:01:20 +0100

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,172 @@
+from abc import ABC, abstractmethod
+
+class Piece(ABC):
+    def __init__(self, color, position):
+        self.__color = color
+        self.__position = position
+
+    @abstractmethod
+    def possible_moves(self):
+        pass
+
+    @staticmethod
+    def is_position_on_board(position):
+        return 1 <= position[0] <= 8 and 1 <= position[1] <= 8
+
+    @property
+    def color(self):
+        return self.__color
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, new_postion):
+        self.__position = new_postion
+
+    def __str__(self):
+        return f'Piece({self.color}) at position {self.position}'
+
+
+class Pawn(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = []
+        
+        if self.color == 'white':
+            new_position = (row + 1, col)
+        else: 
+            new_position = (row - 1, col)
+            
+        if self.is_position_on_board(new_position):
+            moves.append(new_position)
+            
+        return moves
+    
+    def __str__(self):
+        return f'Pawn({self.color}) at position {self.position}'
+
+
+class Knight(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = [
+            (row + 2, col + 1), (row + 2, col - 1),
+            (row - 2, col + 1), (row - 2, col - 1),
+            (row + 1, col + 2), (row + 1, col - 2),
+            (row - 1, col + 2), (row - 1, col - 2)
+        ]
+        final_moves = []
+        for move in moves:
+            if self.is_position_on_board(move):
+                final_moves.append(move)
+        return final_moves
+
+    def __str__(self):
+        return f'Knight({self.color}) at position {self.position}'
+
+
+class Bishop(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = []
+        
+        directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+        
+        for dr, dc in directions:
+            for i in range(1, 8):
+                new_pos = (row + dr * i, col + dc * i)
+                if self.is_position_on_board(new_pos):
+                    moves.append(new_pos)
+                else:
+                    break
+        return moves
+
+    def __str__(self):
+        return f'Bishop({self.color}) at position {self.position}'
+
+
+class Rook(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = []
+        
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        for dr, dc in directions:
+            for i in range(1, 8):
+                new_pos = (row + dr * i, col + dc * i)
+                if self.is_position_on_board(new_pos):
+                    moves.append(new_pos)
+                else:
+                    break
+        return moves
+
+    def __str__(self):
+        return f'Rook({self.color}) at position {self.position}'
+
+
+class Queen(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = []
+        
+        directions = [
+            (1, 0), (-1, 0), (0, 1), (0, -1),
+            (1, 1), (1, -1), (-1, 1), (-1, -1)
+        ]
+        
+        for dr, dc in directions:
+            for i in range(1, 8):
+                new_pos = (row + dr * i, col + dc * i)
+                if self.is_position_on_board(new_pos):
+                    moves.append(new_pos)
+                else:
+                    break
+        return moves
+
+    def __str__(self):
+        return f'Queen({self.color}) at position {self.position}'
+
+
+class King(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = [
+            (row + 1, col), (row - 1, col),
+            (row, col + 1), (row, col - 1),
+            (row + 1, col + 1), (row + 1, col - 1),
+            (row - 1, col + 1), (row - 1, col - 1)
+        ]
+        
+        final_moves = []
+        for move in moves:
+            if self.is_position_on_board(move):
+                final_moves.append(move)
+        return final_moves
+
+    def __str__(self):
+        return f'King({self.color}) at position {self.position}'
+
+
+if __name__ == "__main__":
+    print("--- Knight ---")
+    knight = Knight("black", (1, 2))
+    print(knight)
+    print(knight.possible_moves())
+    
+    print("\n--- Pawn (White) ---")
+    white_pawn = Pawn("white", (2, 2))
+    print(white_pawn)
+    print(white_pawn.possible_moves()) 
+
+    print("\n--- Pawn (Black) ---")
+    black_pawn = Pawn("black", (7, 2))
+    print(black_pawn)
+    print(black_pawn.possible_moves()) 
+
+    print("\n--- King ---")
+    king = King("white", (1, 1)) 
+    print(king)
+    print(king.possible_moves())
