2abfe0f2b4b3170ee1bf3bc4a5fda611468bb11d
Bašta <135508315+Basta05@users.noreply.github.com>
2025-12-10 13:28:48 +0100

    Add files via upload

---
+++ eighth.py
@@ -0,0 +1,11 @@
+def bin_to_dec(binarni_cislo):
+    return int(str(binarni_cislo), 2)
+
+
+def test_bin_to_dec():
+    assert bin_to_dec("0") == 0
+    assert bin_to_dec(1) == 1
+    assert bin_to_dec("100") == 4
+    assert bin_to_dec(101) == 5
+    assert bin_to_dec("010101") == 21
+    assert bin_to_dec(10000000) == 128
